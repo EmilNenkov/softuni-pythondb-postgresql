85ffbd89e5728e598f00d5de7951a24786b6b9bd
Emil Nenkov <enenkov1337@gmail.com>
2023-10-07 00:01:55 +0300

    Create 04-maximum-salary-by-department.sql

---
+++ data-aggregation-lab/04-maximum-salary-by-department.sql
@@ -0,0 +1,14 @@
+SELECT 
+	departments.id AS department_id, 
+	MAX(
+		employees.salary
+	) AS employee_count
+FROM departments
+INNER JOIN employees 
+	ON 
+employees.department_id = departments.id
+GROUP BY
+	departments.id,
+	employees.department_id
+ORDER BY 
+	department_id;
